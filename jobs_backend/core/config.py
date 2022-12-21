76c087e7689fa4ad5f493e76f1ad79456f10970c
amaym0n <loco.skitls@gmail.com>
2022-12-21 22:25:27 +0300

    Fixed DB names for different services

--- jobs_backend/core/config.py
+++ jobs_backend/core/config.py
@@ -18,7 +18,7 @@ class Settings:
     POSTGRES_PASSWORD = os.getenv(key='POSTGRES_PASSWORD')
     POSTGRES_SERVER = os.getenv(key='POSTGRES_SERVER', default='localhost')
     POSTGRES_PORT = os.getenv(key='POSTGRES_PORT', default='5432')
-    POSTGRES_DB = os.getenv(key='POSTGRES_DB')
+    POSTGRES_DB = os.getenv(key='POSTGRES_DB_JOBS')
     POSTGRES_CONN_STRING = \
         f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
